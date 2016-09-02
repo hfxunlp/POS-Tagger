95f89e21ef97d2a2009f3b008d3f919f2fab016f
ano <anoidgit@users.noreply.github.com>
2016-09-02 08:18:30 +0800

    Create conf.lua

---
+++ conf.lua
@@ -0,0 +1,10 @@
+starterate=1000000--warning:only used as init erate, not asigned to criterion
+ieps=8
+warmcycle=4
+expdecaycycle=4
+gtraincycle=64
+modlr=0.5
+csave=3
+nfeature=128
+nhidden=64
+nclass=43
